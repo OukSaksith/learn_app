bfb8e2e678fc340dd59b06fdf2555f9d6e756729
Ouk Saksith <ouksaksith11@gmail.com>
2025-03-24 15:15:23 +0700

    build layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
+    const String appTitle = 'Build Layout';
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
@@ -17,28 +17,16 @@ class MyApp extends StatelessWidget {
         body: const SingleChildScrollView(
           child: Column(
             children: [
-              TitleSection(
-                name: 'Angkor Wat Temple',
-                location: 'Siemreap, Cambodia',
+              ImageSection(
+                image: 'images/angkor.jpg',
               ),
               TitleSection(
                 name: 'Angkor Wat Temple',
                 location: 'Siemreap, Cambodia',
               ),
-              TitleSection(
-                name: 'Angkor Wat Temple',
-                location: 'Siemreap, Cambodia',
-              ),
-              TitleSection(
-                name: 'Angkor Wat Temple',
-                location: 'Siemreap, Cambodia',
-              ),
-              TitleSection(
-                name: 'Angkor Wat Temple',
-                location: 'Siemreap, Cambodia',
-              ),
+              ButtonSection(),
+              TextSection(description: "Angkor Wat is the largest and most iconic temple complex in Cambodia, and one of the most significant archaeological sites in the world. Originally built in the early 12th century by King Suryavarman II, it was initially dedicated to the Hindu god Vishnu before later transitioning into a Buddhist temple."),
             ],
-
           ),
         ),
       ),
@@ -73,10 +61,90 @@ class TitleSection extends StatelessWidget {
               ],
             ),
           ),
-          Icon(Icons.star, color: Colors.red[500]),
+          Icon(Icons.star, color: Colors.pink[500]),
           const Text('99'),
         ],
       ),
     );
   }
 }
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColorDark;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
+          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
+          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.description});
+
+  final String description;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(description, softWrap: true),
+    );
+  }
+  
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(image, width: 600, height: 240, fit: BoxFit.cover);
+  }
+  
+} 

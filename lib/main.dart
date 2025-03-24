2a6e5557be8124ba732d08f355e0cb834632e7e9
Ouk Saksith <ouksaksith11@gmail.com>
2025-03-24 14:29:54 +0700

    body using SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,9 +14,68 @@ class MyApp extends StatelessWidget {
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(title: const Text(appTitle)),
-        body: const Center(
-          child: Text('Hello World'),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              TitleSection(
+                name: 'Angkor Wat Temple',
+                location: 'Siemreap, Cambodia',
+              ),
+              TitleSection(
+                name: 'Angkor Wat Temple',
+                location: 'Siemreap, Cambodia',
+              ),
+              TitleSection(
+                name: 'Angkor Wat Temple',
+                location: 'Siemreap, Cambodia',
+              ),
+              TitleSection(
+                name: 'Angkor Wat Temple',
+                location: 'Siemreap, Cambodia',
+              ),
+              TitleSection(
+                name: 'Angkor Wat Temple',
+                location: 'Siemreap, Cambodia',
+              ),
+            ],
+
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key, required this.name, required this.location});
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(location, style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('99'),
+        ],
       ),
     );
   }
